062ea9f181c95d67cb9d8640aaf242f563616694
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-06-24 18:03:37 -0400

    Added gamemode for CTF

---
+++ ctf/src/gamemode.rs
@@ -0,0 +1,67 @@
+
+use server::*;
+
+use rand;
+use specs::Entity;
+
+use std::cmp::Ordering;
+
+const RED_TEAM: Team = Team(2);
+const BLUE_TEAM: Team = Team(1);
+
+lazy_static! {
+	static ref RED_TEAM_RESPAWN: Position = Position::default();
+	static ref BLUE_TEAM_RESPAWN: Position = Position::default();
+}
+
+#[derive(Default, Debug)]
+pub struct CTFGameMode {
+	redteam: u16,
+	blueteam: u16,
+}
+
+impl CTFGameMode {
+	pub fn new() -> Self {
+		Self::default()
+	}
+}
+
+impl GameMode for CTFGameMode {
+	fn assign_team(&mut self, _: Entity) -> Team {
+		match self.redteam.cmp(&self.blueteam) {
+			Ordering::Less => {
+				self.redteam += 1;
+				RED_TEAM
+			},
+			Ordering::Greater => {
+				self.blueteam += 1;
+				BLUE_TEAM
+			},
+			Ordering::Equal => {
+				let team: bool = rand::random();
+
+				if team {
+					self.redteam += 1;
+					RED_TEAM
+				}
+				else {
+					self.blueteam += 1;
+					BLUE_TEAM
+				}
+			}
+		}
+	}
+
+	fn respawn_pos(&mut self, _: Entity, team: Team) -> Position {
+		if team == BLUE_TEAM {
+			*BLUE_TEAM_RESPAWN
+		}
+		else if team == RED_TEAM {
+			*RED_TEAM_RESPAWN
+		}
+		else {
+			// No need for this yet
+			unimplemented!();
+		}
+	}
+}
--- ctf/src/main.rs
+++ ctf/src/main.rs
@@ -7,22 +7,27 @@ extern crate lazy_static;
 #[macro_use]
 extern crate log;
 
-extern crate airmash_server;
 extern crate fnv;
-extern crate htmlescape;
+extern crate rand;
 extern crate shred;
 extern crate shrev;
 extern crate specs;
+extern crate htmlescape;
+extern crate airmash_server;
 
 use airmash_server as server;
 
 mod component;
 mod systems;
+mod gamemode;
 
 use server::AirmashServer;
+use gamemode::CTFGameMode;
 
 fn main() {
-    let mut server = AirmashServer::new("0.0.0.0:3501").with_engine();
+    let mut server = AirmashServer::new("0.0.0.0:3501")
+			.with_engine()
+			.with_gamemode(CTFGameMode::new());
 
     server.builder = systems::register(&mut server.world, server.builder);
 
