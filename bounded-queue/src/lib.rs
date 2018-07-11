f7998c28f2e55daeca7d3126091046d95cf400c5
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-07-11 19:32:41 -0400

    Formatted bounded-queue

--- bounded-queue/src/lib.rs
+++ bounded-queue/src/lib.rs
@@ -4,68 +4,72 @@ use std::mem;
 
 #[derive(Clone, Debug)]
 pub struct BoundedQueue<T> {
-	front: usize,
-	back: usize,
-	vals: Box<[Option<T>]>
+    front: usize,
+    back: usize,
+    vals: Box<[Option<T>]>,
 }
 
 impl<T> BoundedQueue<T> {
-	pub fn len(&self) -> usize {
-		self.front - self.back
-	}
-	pub fn is_empty(&self) -> bool {
-		self.front == self.back
-	}
-	pub fn is_full(&self) -> bool {
-		self.len() == self.capacity()
-	}
-	pub fn capacity(&self) -> usize {
-		self.vals.len()
-	}
+    pub fn len(&self) -> usize {
+        self.front - self.back
+    }
+    pub fn is_empty(&self) -> bool {
+        self.front == self.back
+    }
+    pub fn is_full(&self) -> bool {
+        self.len() == self.capacity()
+    }
+    pub fn capacity(&self) -> usize {
+        self.vals.len()
+    }
 
-	fn last(&self) -> usize {
-		self.back % self.capacity()
-	}
-	fn first(&self) -> usize {
-		self.front % self.capacity()
-	}
+    fn last(&self) -> usize {
+        self.back % self.capacity()
+    }
+    fn first(&self) -> usize {
+        self.front % self.capacity()
+    }
 
-	pub fn push(&mut self, val: T) -> Option<T> {
-		let val = mem::replace(&mut self.vals[self.first()], Some(val));
+    pub fn push(&mut self, val: T) -> Option<T> {
+        let val = mem::replace(&mut self.vals[self.first()], Some(val));
 
-		if self.is_full() {
-			self.back += 1;
-		}
-		self.front += 1;
+        if self.is_full() {
+            self.back += 1;
+        }
+        self.front += 1;
 
-		val
-	}
+        val
+    }
 
-	pub fn pop(&mut self) -> Option<T> {
-		if self.is_empty() { return None; }
+    pub fn pop(&mut self) -> Option<T> {
+        if self.is_empty() {
+            return None;
+        }
 
-		let last = self.last();
-		self.back += 1;
+        let last = self.last();
+        self.back += 1;
 
-		mem::replace(&mut self.vals[last], None)
-	}
-	pub fn peek(&self) -> Option<&T> {
-		if self.is_empty() { return None; }
+        mem::replace(&mut self.vals[last], None)
+    }
+    pub fn peek(&self) -> Option<&T> {
+        if self.is_empty() {
+            return None;
+        }
 
-		Some(self.vals[self.last()].as_ref().unwrap())
-	}
+        Some(self.vals[self.last()].as_ref().unwrap())
+    }
 }
 
 impl<T: Clone> BoundedQueue<T> {
-	pub fn new(size: usize) -> Self {
-		if size == 0 {
-			panic!("A bounded queue may not be constructed with a size of 0!");
-		}
+    pub fn new(size: usize) -> Self {
+        if size == 0 {
+            panic!("A bounded queue may not be constructed with a size of 0!");
+        }
 
-		Self {
-			front: 0,
-			back: 0,
-			vals: vec![None; size].into_boxed_slice()
-		}
-	}
+        Self {
+            front: 0,
+            back: 0,
+            vals: vec![None; size].into_boxed_slice(),
+        }
+    }
 }
--- bounded-queue/tests/tests.rs
+++ bounded-queue/tests/tests.rs
@@ -1,40 +1,39 @@
-
 #![feature(test)]
 
-extern crate test;
 extern crate bounded_queue;
+extern crate test;
 
 use bounded_queue::BoundedQueue;
 
 #[test]
 #[should_panic]
 pub fn cannot_create_zero_sized_queue() {
-	let _ = BoundedQueue::<u8>::new(0);
+    let _ = BoundedQueue::<u8>::new(0);
 }
 
 #[test]
 pub fn empty_queue_is_empty() {
-	let x = BoundedQueue::<u8>::new(5);
+    let x = BoundedQueue::<u8>::new(5);
 
-	assert!(x.is_empty());
+    assert!(x.is_empty());
 }
 
 #[test]
 pub fn push_on_full_returns_some() {
-	let mut x = BoundedQueue::<u8>::new(1);
+    let mut x = BoundedQueue::<u8>::new(1);
 
-	assert!(x.push(0).is_none());
-	assert!(x.push(1).is_some());
+    assert!(x.push(0).is_none());
+    assert!(x.push(1).is_some());
 }
 
 #[test]
 pub fn peek_on_empty_returns_none() {
-	let x = BoundedQueue::<u8>::new(1);
-	assert!(x.peek().is_none());
+    let x = BoundedQueue::<u8>::new(1);
+    assert!(x.peek().is_none());
 }
 
 #[test]
 pub fn pop_on_empty_returns_none() {
-	let mut x = BoundedQueue::<u8>::new(1);
-	assert!(x.pop().is_none());
+    let mut x = BoundedQueue::<u8>::new(1);
+    assert!(x.pop().is_none());
 }
