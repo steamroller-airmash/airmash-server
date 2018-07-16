7f0fdf03c7b95be9b12d908bfc15cf517809c3bb
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-07-16 19:52:57 -0400

    Format base

--- base/src/main.rs
+++ base/src/main.rs
@@ -12,27 +12,27 @@ use specs::Entity;
 struct EmptyGameMode;
 
 impl GameMode for EmptyGameMode {
-    fn assign_team(&mut self, player: Entity) -> Team {
-        Team(player.id() as u16)
-    }
-    fn spawn_pos(&mut self, _: Entity, _: Team) -> Position {
-        Position::default()
-    }
-    fn gametype(&self) -> GameType {
-        GameType::FFA
-    }
-    fn room(&self) -> String {
-        "matrix".to_owned()
-    }
+	fn assign_team(&mut self, player: Entity) -> Team {
+		Team(player.id() as u16)
+	}
+	fn spawn_pos(&mut self, _: Entity, _: Team) -> Position {
+		Position::default()
+	}
+	fn gametype(&self) -> GameType {
+		GameType::FFA
+	}
+	fn room(&self) -> String {
+		"matrix".to_owned()
+	}
 }
 
 fn main() {
-    simple_logger::init_with_level(log::Level::Info).unwrap();
+	simple_logger::init_with_level(log::Level::Info).unwrap();
 
-    env::set_var("RUST_BACKTRACE", "1");
+	env::set_var("RUST_BACKTRACE", "1");
 
-    AirmashServer::new("0.0.0.0:3501")
-        .with_engine()
-        .with_gamemode(EmptyGameMode)
-        .run();
+	AirmashServer::new("0.0.0.0:3501")
+		.with_engine()
+		.with_gamemode(EmptyGameMode)
+		.run();
 }
