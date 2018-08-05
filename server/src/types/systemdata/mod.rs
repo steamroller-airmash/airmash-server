fee7a760e09169bbd18fa9ed7b53705ec71e6b64
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-08-04 20:21:03 -0400

    Added PlayerRespawner system data wrapper

--- server/src/types/systemdata/mod.rs
+++ server/src/types/systemdata/mod.rs
@@ -4,7 +4,9 @@
 mod clock;
 pub(crate) mod fire_missiles;
 mod isalive;
+mod respawn;
 
 pub use self::clock::ReadClock;
 pub use self::fire_missiles::FireMissiles;
 pub use self::isalive::IsAlive;
+pub use self::respawn::PlayerRespawner;
