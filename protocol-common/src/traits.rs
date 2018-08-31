b672896456a5b2d4dba087df8c286ef9b59dda2b
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-08-31 17:16:37 -0400

    Document traits

--- protocol-common/src/traits.rs
+++ protocol-common/src/traits.rs
@@ -1,22 +1,47 @@
 use {ClientPacket, ServerPacket};
 
-use std::fmt::Display;
+use std::error::Error;
 
+type ServerPacketIterator = Box<Iterator<Item = Vec<u8>>>;
+
+/// Interface to implement for all protocols.
 pub trait Protocol: Sync + Send {
-	type SerializeError: Display;
-	type DeserializeError: Display;
+	type SerializeError: Error;
+	type DeserializeError: Error;
 
+	/// Unique version number for the protocol.
 	fn version(&self) -> u8;
 
-	fn serialize_client(&self, packet: &ClientPacket) -> Result<Vec<u8>, Self::SerializeError>;
-	fn serialize_server(&self, packet: &ServerPacket) -> Result<Vec<u8>, Self::SerializeError>;
+	fn serialize_client(
+		&self,
+		packet: &ClientPacket,
+	) -> Result<ServerPacketIterator, Self::SerializeError>;
+	fn serialize_server(
+		&self,
+		packet: &ServerPacket,
+	) -> Result<ServerPacketIterator, Self::SerializeError>;
 
 	fn deserialize_client(&self, data: &[u8]) -> Result<ClientPacket, Self::DeserializeError>;
 	fn deserialize_server(&self, data: &[u8]) -> Result<ServerPacket, Self::DeserializeError>;
 }
 
+/// Helper trait to make working with protocols easier.
+///
+/// This allows for [`ServerPacket`][0] and
+/// [`ClientPacket`][1] to be serialized and deserialized
+/// without using the `serialize_*` or `deserialize_*`
+/// methods within the [`Protocol`] trait. Instead they
+/// can use [`serialize`][2] and [`deserialize`][3] and
+/// let type inference deduce the correct type.
+///
+/// **This trait should not be implemented by client code.**
+///
+/// [0]: enum.ServerPacket.html
+/// [1]: enum.ClientPacket.html
+/// [2]: #tymethod.serialize
+/// [3]: #tymethod.deserialize
 pub trait ProtocolSerializationExt<T>: Protocol {
-	fn serialize<U>(&self, packet: U) -> Result<Vec<u8>, Self::SerializeError>
+	fn serialize<U>(&self, packet: U) -> Result<ServerPacketIterator, Self::SerializeError>
 	where
 		U: Into<T>;
 
@@ -27,7 +52,7 @@ impl<T> ProtocolSerializationExt<ServerPacket> for T
 where
 	T: Protocol + Sync + Send,
 {
-	fn serialize<U>(&self, packet: U) -> Result<Vec<u8>, Self::SerializeError>
+	fn serialize<U>(&self, packet: U) -> Result<ServerPacketIterator, Self::SerializeError>
 	where
 		U: Into<ServerPacket>,
 	{
@@ -43,7 +68,7 @@ impl<T> ProtocolSerializationExt<ClientPacket> for T
 where
 	T: Protocol + Sync + Send,
 {
-	fn serialize<U>(&self, packet: U) -> Result<Vec<u8>, Self::SerializeError>
+	fn serialize<U>(&self, packet: U) -> Result<ServerPacketIterator, Self::SerializeError>
 	where
 		U: Into<ClientPacket>,
 	{
