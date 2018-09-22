f82cf923c86adf9cfeaac93cafce91c4618c1d98
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-09-22 01:06:47 -0400

    Remove old systems

--- ctf/src/systems/mod.rs
+++ ctf/src/systems/mod.rs
@@ -1,8 +1,5 @@
 mod drop;
-mod drop_on_death;
 mod drop_on_despawn;
-mod drop_on_respawn;
-mod drop_on_spec;
 mod drop_on_stealth;
 mod flagspeed;
 mod pickupflag;
@@ -21,9 +18,7 @@ pub mod timer;
 pub use self::register::register;
 
 pub use self::drop::DropSystem;
-pub use self::drop_on_death::DropOnDeath;
-pub use self::drop_on_respawn::DropOnRespawn;
-pub use self::drop_on_spec::DropOnSpec;
+pub use self::drop_on_despawn::DropOnDespawn;
 pub use self::drop_on_stealth::DropOnStealth;
 pub use self::flagspeed::FlagSpeedSystem;
 pub use self::pickupflag::PickupFlagSystem;
