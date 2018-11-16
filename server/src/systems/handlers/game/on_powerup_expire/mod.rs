2f2742c966efe616603f068523edc78a9b2bb8ef
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-11-16 18:04:36 -0500

    Fix system dependency sanity test

--- server/src/systems/handlers/game/on_powerup_expire/mod.rs
+++ server/src/systems/handlers/game/on_powerup_expire/mod.rs
@@ -2,8 +2,6 @@ mod force_update;
 
 pub use self::force_update::ForceUpdate;
 
-pub type AllEventHandlers = (
-	ForceUpdate,
-);
+pub type AllEventHandlers = (ForceUpdate,);
 
 pub type KnownEventSources = ();
--- server/tests/test-no-loops.rs
+++ server/tests/test-no-loops.rs
@@ -1,9 +1,10 @@
-
 extern crate airmash_server;
 
 use airmash_server::AirmashServer;
 
 #[test]
 fn no_system_dependency_loops() {
-	let _ = AirmashServer::new("0.0.0.0:3501").with_engine();
+	AirmashServer::new("0.0.0.0:3501")
+		.with_engine()
+		.test_build_systems();
 }
