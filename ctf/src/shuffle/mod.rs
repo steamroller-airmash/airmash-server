2f08259d1bd47b9cb5ad42980fad0c68308897a3
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-11-23 01:26:54 -0500

    Switch default shuffle to an alternating shuffle

--- ctf/src/shuffle/mod.rs
+++ ctf/src/shuffle/mod.rs
@@ -2,10 +2,12 @@ mod even_shuffle;
 mod no_shuffle;
 mod random_shuffle;
 mod structs;
+mod alternating_shuffle;
 
 pub use self::even_shuffle::EvenShuffle;
 pub use self::no_shuffle::NoShuffle;
 pub use self::random_shuffle::RandomShuffle;
+pub use self::alternating_shuffle::AlternatingShuffle;
 
 pub use self::structs::{PlayerShuffleInfo, TeamChangeEntry};
 
@@ -14,5 +16,5 @@ pub trait ShuffleProvider {
 }
 
 pub fn get_shuffle() -> Box<ShuffleProvider + Sync + Send> {
-	Box::new(EvenShuffle)
+	Box::new(AlternatingShuffle)
 }
