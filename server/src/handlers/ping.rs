8154d811ff3e31d02730a65d2a96c77cf9cd3855
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-11-24 02:25:33 -0500

    Change ping packets to only be sent once every 5s

--- server/src/handlers/ping.rs
+++ server/src/handlers/ping.rs
@@ -2,62 +2,52 @@ use specs::*;
 use types::systemdata::*;
 use types::*;
 
-use consts::timer::PING_DISPATCH;
+use std::time::{Instant, Duration};
 
 use protocol::server::Ping as ServerPing;
-
-use component::channel::{OnTimerEvent, OnTimerEventReader};
 use systems::handlers::game::on_join::SendLogin;
 
 pub struct PingTimerHandler {
-	reader: Option<OnTimerEventReader>,
+	lastping: Instant,
 }
 
 #[derive(SystemData)]
 pub struct PingTimerHandlerData<'a> {
-	pub entities: Entities<'a>,
-	pub conns: Read<'a, Connections>,
-	pub channel: Read<'a, OnTimerEvent>,
-	pub clock: ReadClock<'a>,
+	entities: Entities<'a>,
+	conns: Read<'a, Connections>,
+	clock: ReadClock<'a>,
 }
 
 impl PingTimerHandler {
 	pub fn new() -> Self {
-		Self { reader: None }
+		Self { lastping: Instant::now() }
 	}
 }
 
 impl<'a> System<'a> for PingTimerHandler {
 	type SystemData = (PingTimerHandlerData<'a>, WriteStorage<'a, PingData>);
 
-	fn setup(&mut self, res: &mut Resources) {
-		self.reader = Some(res.fetch_mut::<OnTimerEvent>().register_reader());
-
-		Self::SystemData::setup(res);
-	}
-
 	fn run(&mut self, (data, mut pingdata): Self::SystemData) {
-		let clock = data.clock.get();
-
-		for evt in data.channel.read(self.reader.as_mut().unwrap()) {
-			if evt.ty == *PING_DISPATCH {
-				continue;
-			}
-
-			(&*data.entities, &mut pingdata)
-				.join()
-				.for_each(|(ent, pingdata)| {
-					let ping = pingdata.new_ping(data.clock.frame.0);
-
-					data.conns.send_to_player(
-						ent,
-						ServerPing {
-							clock,
-							num: ping.idx,
-						},
-					);
-				});
+		if data.clock.frame.0 < self.lastping + Duration::from_secs(5) {
+			return;
 		}
+		self.lastping = data.clock.frame.0;
+
+		let clock = data.clock.get();
+		(&*data.entities, &mut pingdata)
+			.join()
+			.for_each(|(ent, pingdata)| {
+				let ping = pingdata.new_ping(data.clock.frame.0);
+
+				data.conns.send_to_player(
+					ent,
+					ServerPing {
+						clock,
+						num: ping.idx,
+					},
+				);
+			});
+		
 	}
 }
 
