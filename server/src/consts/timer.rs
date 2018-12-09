8b51e88131922e6ecd7306b58bb7fa40ebd2ded5
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-12-09 01:35:22 -0500

    Remove unused PING_DISPATCH timer

--- server/src/consts/timer.rs
+++ server/src/consts/timer.rs
@@ -2,7 +2,6 @@ use utils::timer::*;
 
 lazy_static! {
 	pub static ref INVALID: TimerEventType = TimerEventType::register();
-	pub static ref PING_DISPATCH: TimerEventType = TimerEventType::register();
 	pub static ref AFK_TIMER: TimerEventType = TimerEventType::register();
 	pub static ref SCORE_BOARD: TimerEventType = TimerEventType::register();
 	pub static ref RESPAWN_TIME: TimerEventType = TimerEventType::register();
