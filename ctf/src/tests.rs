d01b73bb6d850c93865fa9d6e584186cccb62639
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-12-18 14:28:29 -0500

    Formatting

--- ctf/src/tests.rs
+++ ctf/src/tests.rs
@@ -1,11 +1,10 @@
-
 use super::*;
 
 /// Tests for loops in system dependencies
 #[test]
 fn test_no_loops() {
 	let mut config = AirmashServerConfig::new_no_gamemode("0.0.0.0:3501").with_engine();
-  config.builder = systems::register(&mut config.world, config.builder);
+	config.builder = systems::register(&mut config.world, config.builder);
 
 	config.builder.build();
 }
