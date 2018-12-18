411efb8207abe960989f4c81e7d40787318a8680
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-12-17 19:51:15 -0500

    Format base main

--- base/src/main.rs
+++ base/src/main.rs
@@ -1,8 +1,8 @@
 extern crate airmash_server;
-extern crate env_logger;
-extern crate specs;
-extern crate serde_json;
 extern crate clap;
+extern crate env_logger;
+extern crate serde_json;
+extern crate specs;
 #[macro_use]
 extern crate log;
 
@@ -32,11 +32,11 @@ impl GameMode for EmptyGameMode {
 
 fn main() {
 	let matches = clap::App::new("airmash-server-ffa")
-			.version(env!("CARGO_PKG_VERSION"))
-			.author("STEAMROLLER")
-			.about("Airmash FFA server")
-			.args_from_usage("-c, --config=[FILE] 'Provides an alternate config file'")
-			.get_matches();
+		.version(env!("CARGO_PKG_VERSION"))
+		.author("STEAMROLLER")
+		.about("Airmash FFA server")
+		.args_from_usage("-c, --config=[FILE] 'Provides an alternate config file'")
+		.get_matches();
 
 	env::set_var("RUST_BACKTRACE", "1");
 	env::set_var("RUST_LOG", "info");
@@ -44,23 +44,23 @@ fn main() {
 	env_logger::init();
 
 	let mut config = AirmashServerConfig::new("0.0.0.0:3501", EmptyGameMode).with_engine();
-	
+
 	if let Some(path) = matches.value_of("config") {
-			let file = match File::open(path) {
-					Ok(x) => x,
-					Err(e) => {
-							eprintln!("Unable to open config file. Error was {}", e);
-							return;
-					}
-			};
+		let file = match File::open(path) {
+			Ok(x) => x,
+			Err(e) => {
+				eprintln!("Unable to open config file. Error was {}", e);
+				return;
+			}
+		};
 
-			let serverconfig: Config = serde_json::from_reader(file).unwrap_or_else(|e| {
-					error!("Unable to parse config file! Using default config.");
-					error!("Config file error was: {}", e);
-					Default::default()
-			});
+		let serverconfig: Config = serde_json::from_reader(file).unwrap_or_else(|e| {
+			error!("Unable to parse config file! Using default config.");
+			error!("Config file error was: {}", e);
+			Default::default()
+		});
 
-			config.world.add_resource(serverconfig);
+		config.world.add_resource(serverconfig);
 	}
 
 	AirmashServer::new(config).run().unwrap();
