618e61f20f2be8beeebd5e3f842146ccdfee09ed
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2019-01-01 22:14:53 -0500

    Prevent shields from spawning within mountains

--- server/src/systems/powerups/spawn_shield.rs
+++ server/src/systems/powerups/spawn_shield.rs
@@ -4,19 +4,21 @@ use component::channel::OnPowerupSpawn;
 use component::event::PowerupSpawnEvent;
 use component::flag::IsPowerup;
 use component::time::MobDespawnTime;
-use consts::config::MAP_SIZE;
+use consts::config::{MAP_SIZE, POWERUP_RADIUS};
+use types::collision::Terrain;
 use types::*;
 
+use rand::{random, Open01};
 use std::time::{Duration, Instant};
-
-use rand::{self, Open01};
 
 // Chance that a shield will spawn on the map each frame.
 const SPAWN_CHANCE: f32 = 0.02;
 const SHIELD_LIFETIME: u64 = 60;
 
 #[derive(Default)]
-pub struct SpawnShield;
+pub struct SpawnShield {
+	terrain: Terrain,
+}
 
 #[derive(SystemData)]
 pub struct SpawnShieldData<'a> {
@@ -33,19 +35,24 @@ impl<'a> System<'a> for SpawnShield {
 	type SystemData = SpawnShieldData<'a>;
 
 	fn run(&mut self, mut data: Self::SystemData) {
-		if rand::random::<Open01<f32>>().0 > SPAWN_CHANCE {
+		if random::<Open01<f32>>().0 > SPAWN_CHANCE {
 			return;
 		}
 
-		let x = rand::random::<Open01<f32>>().0 * MAP_SIZE.x - MAP_SIZE.x * 0.5;
-		let y = rand::random::<Open01<f32>>().0 * MAP_SIZE.y - MAP_SIZE.y * 0.5;
+		let coords = Vector2::<f32>::new(random::<Open01<f32>>().0, random::<Open01<f32>>().0);
+		let pos = coords * MAP_SIZE - MAP_SIZE * 0.5;
+
+		if self.terrain.buckets.does_collide(pos, POWERUP_RADIUS) {
+			// Don't spawn powerups that collide with the terrain
+			return;
+		}
 
 		let despawn = Instant::now() + Duration::from_secs(SHIELD_LIFETIME);
 
 		let mob = data
 			.entities
 			.build_entity()
-			.with(Position::new(x, y), &mut data.pos)
+			.with(pos, &mut data.pos)
 			.with(Mob::Shield, &mut data.mob)
 			.with(MobDespawnTime(despawn), &mut data.despawn_time)
 			.with(IsPowerup, &mut data.is_powerup)
@@ -53,7 +60,7 @@ impl<'a> System<'a> for SpawnShield {
 
 		data.channel.single_write(PowerupSpawnEvent {
 			mob,
-			pos: Position::new(x, y),
+			pos,
 			despawn,
 			ty: Mob::Shield,
 		});
