c6579459b2c6ddf31ee919ff77c5dddd081e7dca
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2019-07-26 16:24:57 -0400

    Remove systems that were obsoleted by the previous commit

--- server/src/systems/handlers/game/on_join/mod.rs
+++ server/src/systems/handlers/game/on_join/mod.rs
@@ -14,43 +14,20 @@ mod send_login;
 mod send_player_new;
 mod send_player_powerup;
 mod send_score;
-// mod update_players_game;
 
 pub use self::init_connection::InitConnection;
-// pub use self::init_earnings::InitEarnings;
-// pub use self::init_join_time::InitJoinTime;
-// pub use self::init_kill_count::InitKillCounters;
-// pub use self::init_last_repel_time::InitLastRepelTime;
-// pub use self::init_limiters::InitLimiters;
-// pub use self::init_name::InitName;
-// pub use self::init_state::InitState;
-// pub use self::init_stealth_time::InitStealthTime;
-// pub use self::init_traits::InitTraits;
-// pub use self::init_transform::InitTransform;
 pub use self::send_level::SendPlayerLevel;
 pub use self::send_login::SendLogin;
 pub use self::send_player_new::SendPlayerNew;
 pub use self::send_player_powerup::SendPlayerPowerup;
 pub use self::send_score::SendScoreUpdate;
-// pub use self::update_players_game::UpdatePlayersGame;
 
 pub type AllJoinHandlers = (
 	InitConnection,
-	// InitEarnings,
-	// InitJoinTime,
-	// InitKillCounters,
-	// InitLastRepelTime,
-	// InitState,
-	// InitName,
-	// InitLimiters,
-	// InitStealthTime,
-	// InitTraits,
-	// InitTransform,
 	SendPlayerLevel,
 	SendLogin,
 	SendPlayerNew,
 	SendScoreUpdate,
-	// UpdatePlayersGame,
 	SendPlayerPowerup,
 );
 
