06ad6dbc4968d8a349d7f5804ebb85ac4401c64c
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2021-07-04 15:52:23 -0400

    Formatting

--- base/src/main.rs
+++ base/src/main.rs
@@ -11,41 +11,41 @@ use airmash_server::resource::RegionName;
 use airmash_server::*;
 
 fn main() {
-	let matches = clap::App::new("airmash-server-ffa")
-		.version(env!("CARGO_PKG_VERSION"))
-		.author("STEAMROLLER")
-		.about("Airmash FFA server")
-		.args_from_usage("-c, --config=[FILE] 'Provides an alternate config file'")
-		.get_matches();
+  let matches = clap::App::new("airmash-server-ffa")
+    .version(env!("CARGO_PKG_VERSION"))
+    .author("STEAMROLLER")
+    .about("Airmash FFA server")
+    .args_from_usage("-c, --config=[FILE] 'Provides an alternate config file'")
+    .get_matches();
 
-	env::set_var("RUST_BACKTRACE", "1");
-	env::set_var("RUST_LOG", "debug");
+  env::set_var("RUST_BACKTRACE", "1");
+  env::set_var("RUST_LOG", "debug");
 
-	env_logger::init();
+  env_logger::init();
 
-	let mut game = AirmashGame::with_network("0.0.0.0:3501".parse().unwrap());
-	game.resources.insert(RegionName("matrix".to_owned()));
-	game.resources.insert(GameType::FFA);
+  let mut game = AirmashGame::with_network("0.0.0.0:3501".parse().unwrap());
+  game.resources.insert(RegionName("matrix".to_owned()));
+  game.resources.insert(GameType::FFA);
 
-	// Use the FFA scoreboard.
-	airmash_server::system::ffa::register_all(&mut game);
+  // Use the FFA scoreboard.
+  airmash_server::system::ffa::register_all(&mut game);
 
-	if let Some(path) = matches.value_of("config") {
-		let file = match File::open(path) {
-			Ok(x) => x,
-			Err(e) => {
-				eprintln!("Unable to open config file. Error was {}", e);
-				return;
-			}
-		};
+  if let Some(path) = matches.value_of("config") {
+    let file = match File::open(path) {
+      Ok(x) => x,
+      Err(e) => {
+        eprintln!("Unable to open config file. Error was {}", e);
+        return;
+      }
+    };
 
-		let mut config = game.resources.write::<Config>();
-		let mut de = serde_json::Deserializer::new(serde_json::de::IoRead::new(file));
-		if let Err(e) = config.deserialize_over(&mut de) {
-			error!("Unable to parse config file: {}", e);
-			return;
-		}
-	}
+    let mut config = game.resources.write::<Config>();
+    let mut de = serde_json::Deserializer::new(serde_json::de::IoRead::new(file));
+    if let Err(e) = config.deserialize_over(&mut de) {
+      error!("Unable to parse config file: {}", e);
+      return;
+    }
+  }
 
-	game.run_until_shutdown();
+  game.run_until_shutdown();
 }
