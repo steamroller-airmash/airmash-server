26e26e4652897828369809f424a0283b112237d4
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2022-02-26 12:38:52 -0500

    Add utility method to get the clock value from a custom time

--- server-next/src/util/mod.rs
+++ server-next/src/util/mod.rs
@@ -6,7 +6,7 @@ use crate::protocol::{Time, Vector2};
 use crate::resource::*;
 use crate::AirmashGame;
 use nalgebra::vector;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 pub(crate) mod escapes;
 pub mod spectate;
@@ -15,18 +15,23 @@ pub fn convert_time(dur: Duration) -> Time {
   dur.as_secs_f32() * 60.0
 }
 
-pub fn get_current_clock(game: &AirmashGame) -> u32 {
+pub fn get_time_clock(game: &AirmashGame, time: Instant) -> u32 {
   let start_time = game
     .resources
     .get::<StartTime>()
     .expect("StartTime not registered in resources");
+
+  let duration = time.saturating_duration_since(start_time.0);
+  (((duration.as_secs() * 1_000_000) + duration.subsec_micros() as u64) / 10) as u32
+}
+
+pub fn get_current_clock(game: &AirmashGame) -> u32 {
   let this_frame = game
     .resources
     .get::<ThisFrame>()
     .expect("ThisFrame not registered in resources");
 
-  let duration = this_frame.0 - start_time.0;
-  (((duration.as_secs() * 1_000_000) + duration.subsec_micros() as u64) / 10) as u32
+  get_time_clock(game, this_frame.0)
 }
 
 pub fn get_server_upgrades(upgrades: &Upgrades, powerup: &Powerup) -> crate::protocol::Upgrades {
