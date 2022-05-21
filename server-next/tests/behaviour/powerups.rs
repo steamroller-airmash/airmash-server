bae3be68f3dd27edd6b182a1d0d35f0cc8da736d
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2022-05-21 14:54:46 +0200

    Add a test verifying that only one plane gets each powerup

--- server-next/tests/behaviour/powerups.rs
+++ server-next/tests/behaviour/powerups.rs
@@ -1,8 +1,8 @@
 use std::time::Duration;
 
-use server::Vector2;
 use server::component::*;
 use server::test::TestGame;
+use server::Vector2;
 
 #[test]
 fn player_is_upgraded_on_collision_with_upgrade() {
@@ -27,4 +27,32 @@ fn player_is_upgraded_on_collision_with_upgrade() {
   if let Some(data) = &powerup.data {
     assert_eq!(data.ty, PowerupType::Inferno);
   }
+}
+
+#[test]
+fn dual_powerup_collision() {
+  let (mut game, mut mock) = TestGame::new();
+
+  let mut client1 = mock.open();
+  let mut client2 = mock.open();
+
+  let p1 = client1.login("p1", &mut game);
+  let p2 = client2.login("p2", &mut game);
+
+  game.world.get_mut::<Position>(p1).unwrap().0 = Vector2::zeros();
+  game.world.get_mut::<Position>(p2).unwrap().0 = Vector2::zeros();
+
+  game.run_for(Duration::from_secs(4));
+  let powerup = game.spawn_mob(MobType::Inferno, Vector2::zeros(), Duration::from_secs(60));
+  game.run_once();
+
+  assert!(
+    !game.world.contains(powerup),
+    "Powerup was not despawned despite having collided with a player"
+  );
+
+  let p1pow = game.world.get::<Powerup>(p1).unwrap();
+  let p2pow = game.world.get::<Powerup>(p2).unwrap();
+
+  assert!(p1pow.data.is_some() != p2pow.data.is_some());
 }
