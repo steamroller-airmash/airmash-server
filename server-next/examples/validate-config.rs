920a31c6206ad5ffb39930caecc1e3cd118eb5ef
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2022-06-02 17:41:09 +0200

    Add tool to validate airmash configs

--- server-next/examples/validate-config.rs
+++ server-next/examples/validate-config.rs
@@ -1,12 +1,12 @@
 use airmash_server::resource::Config;
-use miette::{Diagnostic, NamedSource, Result, SourceOffset};
+use miette::{Diagnostic, NamedSource, SourceOffset};
 use serde_deserialize_over::DeserializeOver;
 use std::io::Read;
+use std::path::Path;
 use std::{env, path::PathBuf};
 use thiserror::Error;
 
 #[derive(Debug, Error, Diagnostic)]
-#[diagnostic()]
 enum ValidationError {
   #[error("Unable to read config file `{filename}`")]
   FileIoError {
@@ -29,39 +29,50 @@ enum ValidationError {
   },
 }
 
-fn main() -> Result<()> {
+fn validate_one(path: &Path) -> Result<(), ValidationError> {
+  let mut file = std::fs::File::open(&path).map_err(|e| ValidationError::FileIoError {
+    inner: e,
+    filename: path.to_owned(),
+  })?;
+
+  let mut data = String::new();
+  file
+    .read_to_string(&mut data)
+    .map_err(|e| ValidationError::FileIoError {
+      inner: e,
+      filename: path.to_owned(),
+    })?;
+
+  let mut config = Config::default();
+  let mut track = serde_path_to_error::Track::new();
+
+  let mut jd = serde_json::Deserializer::from_str(&data);
+  let de = serde_path_to_error::Deserializer::new(&mut jd, &mut track);
+
+  if let Err(e) = config.deserialize_over(de) {
+    return Err(ValidationError::ValidationError {
+      span: SourceOffset::from_location(&data, e.line(), e.column()),
+      source: NamedSource::new(path.display().to_string(), data),
+      path: track.path(),
+      error: e,
+    });
+  }
+
+  Ok(())
+}
+
+fn main() {
   let mut count = 0;
+  let mut failures = Vec::new();
 
   for arg in env::args_os().skip(1) {
     count += 1;
     let path = PathBuf::from(arg);
+    if let Err(e) = validate_one(&path) {
+      let report = miette::Report::from(e);
+      failures.push(path);
 
-    let mut file = std::fs::File::open(&path).map_err(|e| ValidationError::FileIoError {
-      inner: e,
-      filename: path.clone(),
-    })?;
-
-    let mut data = String::new();
-    file
-      .read_to_string(&mut data)
-      .map_err(|e| ValidationError::FileIoError {
-        inner: e,
-        filename: path.clone(),
-      })?;
-
-    let mut config = Config::default();
-    let mut track = serde_path_to_error::Track::new();
-
-    let mut jd = serde_json::Deserializer::from_str(&data);
-    let de = serde_path_to_error::Deserializer::new(&mut jd, &mut track);
-
-    if let Err(e) = config.deserialize_over(de) {
-      Err(ValidationError::ValidationError {
-        span: SourceOffset::from_location(&data, e.line(), e.column()),
-        source: NamedSource::new(path.display().to_string(), data),
-        path: track.path(),
-        error: e,
-      })?
+      eprintln!("{:?}", report);
     }
   }
 
@@ -70,5 +81,13 @@ fn main() -> Result<()> {
     eprintln!("USAGE: validate-config <config-file>...");
   }
 
-  Ok(())
+  if !failures.is_empty() {
+    eprintln!("Error: Validation failed for {} files:", failures.len());
+
+    for failure in &failures {
+      eprintln!("  - {}", failure.display());
+    }
+
+    std::process::exit(1);
+  }
 }
