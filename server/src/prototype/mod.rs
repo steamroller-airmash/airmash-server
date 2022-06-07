53358791a2fefad82ae8dd5cb02871f600019bdf
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2022-06-07 13:00:27 +0300

    Add a missile prototype

--- server/src/prototype/mod.rs
+++ server/src/prototype/mod.rs
@@ -1,3 +1,11 @@
+//! Prototypes for objects within airmash.
+//!
+//! These define the properties that planes, missiles, mobs, specials, and more
+//! will have within airmash. This is to allow for custom configurations to be
+//! created without having to modify code.
+
+mod missile;
 mod plane;
 
+pub use self::missile::MissilePrototype;
 pub use self::plane::PlanePrototype;
