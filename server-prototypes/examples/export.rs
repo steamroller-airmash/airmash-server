13635989c2cd5f214eea7cecc7e8350c0551a077
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2022-06-16 15:50:03 -0400

    Add export script that runs the patch job

--- server-prototypes/examples/export.rs
+++ server-prototypes/examples/export.rs
@@ -1,8 +1,42 @@
+#[cfg(not(feature = "script"))]
+compile_error!("export example requires the `script` feature to be enabled");
+
+use serde::Deserialize;
 use server_prototypes::GamePrototype;
+use std::io::Write;
 
-fn main() {
-  let prototype = GamePrototype::default();
-  let stdout = std::io::stdout().lock();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let args = std::env::args().skip(1).collect::<Vec<_>>();
+  // let args = vec!["server-prototypes/configs/infinite-fire.lua"];
 
-  serde_json::to_writer_pretty(stdout, &prototype).unwrap();
+  let mut prototype = GamePrototype::default();
+
+  let lua = rlua::Lua::new();
+  lua.context(|lua| -> Result<(), Box<dyn std::error::Error>> {
+    for file in args {
+      let contents = std::fs::read_to_string(file)?;
+      let value = prototype.patch_direct(lua, &contents)?;
+
+      let mut track = serde_path_to_error::Track::new();
+      let de = serde_rlua::Deserializer::new(value);
+      let de = serde_path_to_error::Deserializer::new(de, &mut track);
+
+      match GamePrototype::deserialize(de) {
+        Ok(proto) => prototype = proto,
+        Err(e) => Err(format!(
+          "Error while deserializing field {}: {}",
+          track.path(),
+          e
+        ))?,
+      }
+    }
+
+    Ok(())
+  })?;
+
+  let mut stdout = std::io::stdout().lock();
+  serde_json::to_writer_pretty(&mut stdout, &prototype)?;
+  stdout.write_all(b"\n")?;
+
+  Ok(())
 }
