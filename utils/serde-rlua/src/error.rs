69386d1a9ba43d66577b10a432376976cc032d0a
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2022-06-17 10:49:00 -0400

    Introduce a crate to serialize/deserialize from rlua Values

---
+++ utils/serde-rlua/src/error.rs
@@ -0,0 +1,58 @@
+use std::error::Error as StdError;
+use std::fmt;
+
+use rlua::Error as LuaError;
+use serde::{de, ser};
+
+#[derive(Clone, Debug)]
+pub struct Error(LuaError);
+
+impl From<LuaError> for Error {
+  fn from(e: LuaError) -> Self {
+    Self(e)
+  }
+}
+
+impl From<Error> for LuaError {
+  fn from(e: Error) -> Self {
+    e.0
+  }
+}
+
+impl fmt::Display for Error {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    self.0.fmt(f)
+  }
+}
+
+impl StdError for Error {
+  fn source(&self) -> Option<&(dyn StdError + 'static)> {
+    Some(&self.0)
+  }
+}
+
+impl ser::Error for Error {
+  fn custom<T>(msg: T) -> Self
+  where
+    T: fmt::Display,
+  {
+    Self(LuaError::ToLuaConversionError {
+      from: "serialize",
+      to: "value",
+      message: Some(msg.to_string()),
+    })
+  }
+}
+
+impl de::Error for Error {
+  fn custom<T>(msg: T) -> Self
+  where
+    T: fmt::Display,
+  {
+    Self(LuaError::FromLuaConversionError {
+      from: "value",
+      to: "deserialize",
+      message: Some(msg.to_string()),
+    })
+  }
+}
