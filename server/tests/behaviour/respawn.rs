78b2a90c188c88525400deca2d020ff91f7c7b39
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2022-06-19 22:27:35 -0400

    Sequence `PLAYER_TYPE` messages before `PLAYER_RESPAWN` messages for plane change commands (#202)
    The issue motivating this commit was that Q and E would fail to do anything when playing as a mohawk. The root cause of this issue was due to a bit of a desync between what the server thinks the player's type is and what the client thinks the player's type is. This is because the type of the current player is stored in two places in the client:
    1. `Players.getMe().type`
    2. `game.myType`
    
    `Players.getMe().type` is updated when the client receives a `PLAYER_TYPE` packet for the current player. However, `game.myType` is only updated when the player respawns. This isn't usually noticeable because the _only_ thing that `game.myType` is used for is determining whether Q and E will trigger strafing or do nothing.
    
    The fix, as contained in this commit, is to make sure to send the `PLAYER_TYPE` packet before sending the `PLAYER_RESPAWN` packet. This ensures that the client has the right type before it gets updated in `Player.respawn`.
    
    Fixes #201

--- server/tests/behaviour/respawn.rs
+++ server/tests/behaviour/respawn.rs
@@ -25,3 +25,30 @@ fn respawn_as_mohawk() {
     }
   }
 }
+
+/// This test validates that issue #201 is actually fixed. If we send
+/// PLAYER_TYPE packets after PLAYER_RESPAWN packets then we'll find that the
+/// plane type used by the client for determining whether Q and E are enabled -
+/// and only for that - is one behind what it should be.
+#[test]
+fn player_type_before_player_respawn() {
+  let (mut game, mut mock) = TestGame::new();
+
+  let mut client = mock.open();
+  client.login("test", &mut game);
+
+  game.run_for(Duration::from_secs(2));
+
+  // Drain all previously sent packets
+  let _ = client.packets().count();
+  client.send_command("respawn", "3");
+  game.run_once();
+
+  // Verify that PLAYER_TYPE occurs before PLAYER_RESPAWN
+  assert!(client
+    .packets()
+    .any(|p| matches!(p, ServerPacket::PlayerType(_))));
+  assert!(client
+    .packets()
+    .any(|p| matches!(p, ServerPacket::PlayerRespawn(_))));
+}
