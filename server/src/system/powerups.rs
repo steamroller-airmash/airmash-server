a457014ad83765460e7067e40bd1ec23979650a1
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2022-06-20 21:55:43 -0400

    Use effect component as the source of truth for expiring powerups

--- server/src/system/powerups.rs
+++ server/src/system/powerups.rs
@@ -2,33 +2,38 @@ use smallvec::SmallVec;
 
 use crate::component::*;
 use crate::event::PowerupExpire;
-use crate::resource::ThisFrame;
 use crate::AirmashGame;
 
 pub fn update(game: &mut AirmashGame) {
-  expire_powerups(game);
+  expire_effects(game);
 }
 
-fn expire_powerups(game: &mut AirmashGame) {
-  let this_frame = game.resources.read::<ThisFrame>().0;
-  let query = game.world.query_mut::<&Powerup>().with::<IsPlayer>();
+fn expire_effects(game: &mut AirmashGame) {
+  let this_frame = game.this_frame();
+  let query = game.world.query_mut::<&Effects>().with::<IsPlayer>();
 
   let mut events = SmallVec::<[_; 16]>::new();
-  for (ent, powerup) in query {
-    let powerup = match &powerup.data {
-      Some(data) => data,
-      None => continue,
+  for (ent, effects) in query {
+    match effects.expiry() {
+      Some(expiry) if expiry <= this_frame => (),
+      _ => continue,
     };
 
-    if powerup.end_time > this_frame {
-      continue;
-    }
-
+    info!(
+      "Expiring powerup {:?} on {:?}",
+      effects.powerup().unwrap(),
+      ent
+    );
     events.push(PowerupExpire { player: ent });
   }
 
   for event in events {
     game.dispatch(event);
+    game
+      .world
+      .get_mut::<Effects>(event.player)
+      .unwrap()
+      .clear_powerup();
     game.world.get_mut::<Powerup>(event.player).unwrap().data = None;
   }
 }
