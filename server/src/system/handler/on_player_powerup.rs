17dbe588955057c5158b0370f33600596fed3e13
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2022-06-20 20:28:06 -0400

    Add player powerup handler to add effects component

--- server/src/system/handler/on_player_powerup.rs
+++ server/src/system/handler/on_player_powerup.rs
@@ -11,13 +11,18 @@ fn send_packet(event: &PlayerPowerup, game: &mut AirmashGame) {
     return;
   }
 
-  let duration = event.duration.as_secs() * 1000 + event.duration.subsec_millis() as u64;
+  let (duration, ty) = match (event.powerup.duration, event.powerup.server_type) {
+    (Some(duration), Some(ty)) => (duration, ty),
+    _ => return,
+  };
+
+  let duration = duration.as_secs() * 1000 + duration.subsec_millis() as u64;
 
   game.send_to(
     event.player,
     s::PlayerPowerup {
       duration: duration as u32,
-      ty: event.ty,
+      ty,
     },
   );
 }
@@ -37,4 +42,26 @@ fn update_fields(event: &PlayerPowerup, game: &mut AirmashGame) {
 
   last_update.0 = start_time;
   *powerup = Powerup::new(event.ty, this_frame + event.duration);
+}
+
+#[handler]
+fn update_effects(event: &PlayerPowerup, game: &mut AirmashGame) {
+  let start_time = game.start_time();
+  let this_frame = game.this_frame();
+
+  let (duration, ty) = match (event.powerup.duration, event.powerup.server_type) {
+    (Some(duration), Some(ty)) => (duration, ty),
+    _ => return,
+  };
+
+  let (last_update, effects, _) = match game
+    .world
+    .query_one_mut::<(&mut LastUpdateTime, &mut Effects, &IsPlayer)>(event.player)
+  {
+    Ok(query) => query,
+    Err(_) => return,
+  };
+
+  last_update.0 = start_time;
+  effects.set_powerup(ty, this_frame + duration, &event.powerup.effects);
 }
