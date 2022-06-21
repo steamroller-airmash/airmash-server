0143f70eea2b3643ffc1cfb3dcf9985818ad0c99
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2022-06-21 14:59:12 -0400

    Move upgrade effect handing to on_player_powerup handlers

--- server/src/system/handler/on_player_mob_collision.rs
+++ server/src/system/handler/on_player_mob_collision.rs
@@ -16,72 +16,6 @@ fn dispatch_despawn_event(event: &PlayerMobCollision, game: &mut AirmashGame) {
 }
 
 #[handler(priority = crate::priority::HIGH)]
-fn update_player_upgrades(event: &PlayerMobCollision, game: &mut AirmashGame) {
-  let (&mob, _) = match game
-    .world
-    .query_one_mut::<(&MobPrototypeRef, &IsMob)>(event.mob)
-  {
-    Ok(query) => query,
-    Err(_) => return,
-  };
-
-  if mob.server_type != MobType::Upgrade {
-    return;
-  }
-
-  let (upgrades, prev, _) = match game
-    .world
-    .query_one_mut::<(&mut Upgrades, &mut PrevUpgrades, &IsPlayer)>(event.player)
-  {
-    Ok(query) => query,
-    Err(_) => return,
-  };
-
-  upgrades.unused += 1;
-  prev.0 = *upgrades;
-}
-
-#[handler]
-fn send_player_upgrade(event: &PlayerMobCollision, game: &mut AirmashGame) {
-  use crate::protocol::server::ScoreUpdate;
-
-  let (&mob, _) = match game
-    .world
-    .query_one_mut::<(&MobPrototypeRef, &IsMob)>(event.mob)
-  {
-    Ok(query) => query,
-    Err(_) => return,
-  };
-
-  if mob.server_type != MobType::Upgrade {
-    return;
-  }
-
-  let (upgrades, score, earnings, kills, deaths, _) = match game.world.query_one_mut::<(
-    &Upgrades,
-    &Score,
-    &Earnings,
-    &KillCount,
-    &DeathCount,
-    &IsPlayer,
-  )>(event.player)
-  {
-    Ok(query) => query,
-    Err(_) => return,
-  };
-
-  let packet = ScoreUpdate {
-    id: event.player.id() as _,
-    upgrades: upgrades.unused,
-    score: score.0,
-    earnings: earnings.0,
-    total_kills: kills.0,
-    total_deaths: deaths.0,
-  };
-  game.send_to(event.player, packet);
-}
-
-#[handler(priority = crate::priority::HIGH)]
 fn update_player_powerup(event: &PlayerMobCollision, game: &mut AirmashGame) {
   let (&mob, _) = match game
     .world
@@ -91,10 +25,6 @@ fn update_player_powerup(event: &PlayerMobCollision, game: &mut AirmashGame) {
     Err(_) => return,
   };
 
-  if !mob.server_type.is_powerup() {
-    return;
-  }
-
   let (effects, _) = match game
     .world
     .query_one_mut::<(&Effects, &IsPlayer)>(event.player)
@@ -103,7 +33,7 @@ fn update_player_powerup(event: &PlayerMobCollision, game: &mut AirmashGame) {
     Err(_) => return,
   };
 
-  if effects.expiry().is_some() {
+  if effects.powerup().is_some() {
     game.dispatch(PowerupExpire {
       player: event.player,
     });
