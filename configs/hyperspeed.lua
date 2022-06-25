af60add9d05ed35676bf66b88590e1f8a3675f4c
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2022-06-25 11:39:12 -0400

    Convert JSON configs to LUA configs

---
+++ configs/hyperspeed.lua
@@ -0,0 +1,4 @@
+
+for idx, plane in pairs(data.planes) do
+  plane.max_speed = 30.0
+end
--- configs/lazormissiles.lua
+++ configs/lazormissiles.lua
@@ -1,14 +1,19 @@
 
+for idx, missile in pairs(data.missiles) do
+  missile.max_speed = 30.0
+  missile.base_speed = 30.0
+  missile.distance = 10000.0
+end
+
 -- Set the firing delay and energy for all planes to 0.
 for key, plane in pairs(data.planes) do
-  plane.fire_delay = 0
+  plane.fire_delay = 0.1
   plane.fire_energy = 0
 end
 
--- Also enable infinite fire for tornado multishot
 for key, special in pairs(data.specials) do
   if special.name == "multishot" then
     special.cost = 0
-    special.delay = 0
+    special.delay = 0.1
   end
 end
